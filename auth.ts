d019e3d479b2b9192c7c2d65eb8d4f1ab4460a14
NaveenSandaru <naveensandaru2@gmail.com>
2025-03-10 07:09:47 +0530

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
+//Exporting Auth elements
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
 })
